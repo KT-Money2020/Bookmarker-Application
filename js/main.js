a8672d4bc4f7903312e6291cdc31318d3bfba4c7
Kevin Tran <mai_le09@yahoo.com>
2019-08-04 16:18:37 -0700

    Added event listener for submitting button and a method for staving input

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 
 
 
+//Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
-
+//Save Bookmark
+function saveBookmark(){
+    //Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+    //When you want to store data to localStorage you would most likely use an array of objects to store that data
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+    
+    
+    
+    
+    
+    //This premade method is designed to prevent the page from actually submitting so that we could work with the code
+    e.preventDefault();
+}
 
 
 
