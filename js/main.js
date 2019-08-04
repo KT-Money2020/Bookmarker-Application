75db41ac71df07d832a0f1a2e9a5beea6021d5f7
Kevin Tran <mai_le09@yahoo.com>
2019-08-04 16:21:59 -0700

    Lots of documentation and info + local Storage added for input storage

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 //Save Bookmark
-function saveBookmark(){
+function saveBookmark(e){
     //Get form values
     var siteName = document.getElementById('siteName').value;
     var siteUrl = document.getElementById('siteUrl').value;
@@ -15,9 +15,38 @@ function saveBookmark(){
         url: siteUrl
     }
     
+    //Local Storage stores only strings 
+    //The first string in the local storage method is the name in which you would later call back in order to access the string on the second index which is the data.
+    
+    /*
+    localStorage.setItem('test', 'Hello World');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+    */
     
     
-    
+    //Test if bookmarks is null
+    /*testing to see if the so called 'bookmark' object is empty within the localStorage and if not then its going to create an empty array to add into the storage which will then later be holding all the objects we make. And of course all of these objects and arrays has to be turned into a string in order to be held in local storage */
+    if(localStorage.getItem('booksmarks') === null){
+        //Init array
+        var bookmarks = [];
+        //Add to array
+        bookmarks.push(bookmark);
+        //Set to LocalStorage
+        /*You cannot command the local storage to be set like this as the localStorage only accepts strings, therefore you need to manually turn it into a string using the JSON command as shown below. Cannot do this: localStorage.setItem('bookmarks', bookmarks);*/
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        /* Here is where we will be receiving back the local bookmark array in order to update the object data with new ones */
+        
+        //JSON.parse() turns the string back into JSON aka the array holding the object
+        //Get bookmark from local storage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        //Add bookmark to array 
+        bookmarks.push(bookmark);
+        //Re-set back to localStorage
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
     
     //This premade method is designed to prevent the page from actually submitting so that we could work with the code
     e.preventDefault();
